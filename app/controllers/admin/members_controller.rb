d340fe059f54f22fa01e1f255fde42da7ae19536
taroken65 <yonemurano@gmail.com>
2021-08-19 08:48:43 +0000

    admin_members

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,2 +1,26 @@
 class Admin::MembersController < ApplicationController
+    def index
+     @members = Member.all
+    end
+    
+    def show
+     @member = Member.find(params[:id])
+    end
+    
+    def edit 
+     @member = Member.find(params[:id])
+    end
+    
+    def update
+     member = Member.find(params[:id])
+  	 member.update(member_params)
+     redirect_to member_path(member)
+    end
+    
+ private
+
+  def member_params
+    params.require(:member).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :post_code,:address, :phone_number, :is_deleted)
+  end
+  
 end
